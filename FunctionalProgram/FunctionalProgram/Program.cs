fb624fc37cbb581d9222ae29269a50365e207e69
rahimabd13 <abdulrahim13501@gmail.com>
2023-06-26 12:02:32 +0530

    [Rahim] Add: Calculate Fliping Coin Percentage

---
+++ FunctionalProgram/FunctionalProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FlipCoin flipcoin = new FlipCoin();
+            flipcoin.Percentage();
+        }
+    }
+}
